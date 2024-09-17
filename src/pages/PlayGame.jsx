d5cf0528f01a746c75bb8f8d9a8b9e54c5845e3b
swapnil <swapnil130501@gmail.com>
2024-09-17 10:40:40 +0530

    Hanging logic

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -2,13 +2,22 @@ import React, { useState } from 'react'
 import { useLocation, Link } from "react-router-dom";
 import MaskedText from '../components/MaskedText/MaskedText';
 import LetterButtons from '../components/LetterButtons/LetterButtons';
+import HangMan from '../components/HangMan/HangMan';
 
 function PlayGame() {
     const {state} = useLocation();
 
     const [gussedLetters, setGussedLetters] = useState([]);
-    
+    const [step, setStep] = useState(0);
+
     function handleLetterClick(letter) {
+        if(state.wordSelected.toUpperCase().includes(letter)) {
+
+        }
+        else {
+            setStep(step + 1);
+        }
+
         setGussedLetters([...gussedLetters, letter])
     }
 
@@ -18,6 +27,9 @@ function PlayGame() {
             <MaskedText text={state.wordSelected} gussedLetters = {gussedLetters}/>
             <div>
                 <LetterButtons text={state.wordSelected} gussedLetters = {gussedLetters} onLetterClick={handleLetterClick}></LetterButtons>
+            </div>
+            <div>
+                <HangMan step={step}></HangMan>
             </div>
             <Link to="/start" className="text-blue-400">Start Game</Link>
         </div>
